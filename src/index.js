5dad5021c0a3ef03d62e8c4658c5ca574dfc06eb
Ayush Badoni <ayushbadoni36@gmail.com>
2025-07-21 10:06:59 +0530

    :)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
-import mongoose from "mongoose"
 import express from "express"
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
 
+dotenv.config({path : "./env"});
 const app = express()
 
-;( async function(){
-    try{
-        await mongoose.connect(`${process.env.URI}`);
-        app.on("Error" , (error) => { 
-            console.error("error" , error);
-            throw error;
-        })
-        app.listen(process.env.PORT , ()=>{
-            console.log(`App is running on port: ${process.env.PORT}`);
-        })
-    }catch(error){
-        console.error(error);
-        throw error;
-    } 
-})();
+connectDB()
+
+
+
+
+
+
+//Commented out for my own reference 
+// ;( async function(){
+//     try{
+//         await mongoose.connect(`${process.env.URI}`);
+//         app.on("Error" , (error) => { 
+//             console.error("error" , error);
+//             throw error;
+//         })
+//         app.listen(process.env.PORT , ()=>{
+//             console.log(`App is running on port: ${process.env.PORT}`);
+//         })
+//     }catch(error){
+//         console.error(error);
+//         throw error;
+//     } 
+// })();
